5ab5410c1ba2cfac7cd8e5f12a80c6926beab20f
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2024-12-20 16:33:16 -0300

    repositorio cliente

--- src/app/backend/db/ColecaoCliente.ts
+++ src/app/backend/db/ColecaoCliente.ts
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "../config";
 import Cliente from "../../core/Cliente";
 import ClienteRepositorio from "../../core/ClienteRepositorio";
 
@@ -11,17 +11,35 @@ export default class ColecaoCliente implements ClienteRepositorio {
             idade: cliente.idade,
            } 
         },
-        fromFirestore(snapshot: firebase.firestore.QueryDocumentShapshot, options)
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentShapshot, options: firebase.firestore.SnapshotOptions) : Cliente{
+            const dados = snapshot.data(options)
+            return new Cliente(dados.nome, dados.idade, snapshot?.id)
+        }
     }
     
     async salvar(cliente: Cliente): Promise<Cliente> {
-        return null
+      if(cliente?.id){
+       await this.colecao().doc(cliente.id).set(cliente)
+       return cliente
+      }
+       else {
+        const docRef = await  this.colecao().add(cliente)
+       await doc =  docRef.get()
+       return doc.data()
+        
+      }
+
     }
     async excluir(cliente: Cliente): Promise<void> {
-        return null
+      return this.colecao.doc(cliente.id).delete()
     }
 
     async obterTodos(): Promise<Cliente[]> {
-        return null
+    const query = await this.colecao().get()
+        return query.docs.map(doc => doc.data()) ?? []
+    }
+
+    private colecao(){
+        return firebase.firestore().collection('clientes').withConverter(this.#conversor)
     }
 }
